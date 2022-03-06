7c19746bd10eb0b19f21b4607629db617830895b
Anuth Asokan <anuthkasokan@gmail.com>
2022-03-06 17:23:09 -0500

    s3 bucket

--- amplify/backend/function/paymentfunc/src/app.js
+++ amplify/backend/function/paymentfunc/src/app.js
@@ -6,87 +6,107 @@ or in the "license" file accompanying this file. This file is distributed on an 
 See the License for the specific language governing permissions and limitations under the License.
 */
 
-
-
-
-const express = require('express')
-const bodyParser = require('body-parser')
-const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
+const express = require("express");
+const bodyParser = require("body-parser");
+const awsServerlessExpressMiddleware = require("aws-serverless-express/middleware");
 
 // declare a new express app
-const app = express()
-app.use(bodyParser.json())
-app.use(awsServerlessExpressMiddleware.eventContext())
+const app = express();
+app.use(bodyParser.json());
+app.use(awsServerlessExpressMiddleware.eventContext());
 
 // Enable CORS for all methods
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "*")
-  next()
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
 });
-
 
 /**********************
  * Example get method *
  **********************/
 
-app.get('/payment', function(req, res) {
+app.get("/payment", function (req, res) {
   // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
+  res.json({ success: "get call succeed!", url: req.url });
 });
 
-app.get('/payment/*', function(req, res) {
+app.get("/payment/*", function (req, res) {
   // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
-});
-
-/****************************
-* Example post method *
-****************************/
-
-app.post('/payment', function(req, res) {
-  // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
-});
-
-app.post('/payment/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+  res.json({ success: "get call succeed!", url: req.url });
 });
 
 /****************************
-* Example put method *
-****************************/
+ * Example post method *
+ ****************************/
 
-app.put('/payment', function(req, res) {
+app.post("/payment", async function (req, res) {
   // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+  let { amount, id } = req.body;
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: "USD",
+      description: "Call of Duty 7",
+      payment_method: id,
+      confirm: true,
+    });
+    console.log("payment", payment);
+    res.json({
+      message: "Payment successful",
+      success: true,
+    });
+  } catch (error) {
+    console.log("error", error);
+    res.json({
+      message: "Payment failed",
+      success: false,
+    });
+  }
+  //res.json({ success: "post call succeed!", url: req.url, body: req.body });
 });
 
-app.put('/payment/*', function(req, res) {
+app.post("/payment/*", function (req, res) {
   // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+  res.json({ success: "post call succeed!", url: req.url, body: req.body });
 });
 
 /****************************
-* Example delete method *
-****************************/
+ * Example put method *
+ ****************************/
 
-app.delete('/payment', function(req, res) {
+app.put("/payment", function (req, res) {
   // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
+  res.json({ success: "put call succeed!", url: req.url, body: req.body });
 });
 
-app.delete('/payment/*', function(req, res) {
+app.put("/payment/*", function (req, res) {
   // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
+  res.json({ success: "put call succeed!", url: req.url, body: req.body });
 });
 
-app.listen(3000, function() {
-    console.log("App started")
+/****************************
+ * Example delete method *
+ ****************************/
+
+app.delete("/payment", function (req, res) {
+  // Add your code here
+  res.json({ success: "delete call succeed!", url: req.url });
+});
+
+app.delete("/payment/*", function (req, res) {
+  // Add your code here
+  res.json({ success: "delete call succeed!", url: req.url });
+});
+
+app.listen(3000, function () {
+  console.log("App started");
 });
 
 // Export the app object. When executing the application local this does nothing. However,
 // to port it to AWS Lambda we will create a wrapper around that will load the app from
 // this file
-module.exports = app
+module.exports = app;
