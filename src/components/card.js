82605e1cb94d873fa4b2af4d163b161a772fcd7b
Anuth Asokan <anuthkasokan@gmail.com>
2022-03-07 21:37:25 -0500

    added query scan

--- src/components/card.js
+++ src/components/card.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const CardComponent = (props) => {
-    console.log(props)
-    return (
-        <div className="card" key={props.item.id}>
-            <div className="card_img">
-                <img src={props.item.thumb} />
-            </div>
-            <div clasName="card_header">
-                <h2 className="prod-name">{props.item.product_name}</h2>
-                <p className="prod-desc">{props.item.description}</p>
-                <p className="price">{props.item.price}<span>{props.item.currency}</span></p>
-                <div className="add-btn">Buy</div>
-            </div>
-        </div>
-    )
-}
+  console.log(props);
+  return (
+    <div className="card" key={props.item.videogameId}>
+      <div className="card_img">
+        <img src={props.item.imageUrl} />
+      </div>
+      <div clasName="card_header">
+        <h2 className="prod-name">{props.item.name}</h2>
+        <p className="prod-desc">{props.item.company}</p>
+        <p className="price">
+          {props.item.price}
+          <span>$</span>
+        </p>
+        <div className="add-btn">Buy</div>
+      </div>
+    </div>
+  );
+};
 
 export default CardComponent;
