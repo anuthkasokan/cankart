ed7eed265b8a66d387c25a44a2a9a9ba2b5a5ba9
Anuth Asokan <anuthkasokan@gmail.com>
2022-03-13 17:48:15 -0400

    Update app.js

--- amplify/backend/function/paymentfunc/src/app.js
+++ amplify/backend/function/paymentfunc/src/app.js
@@ -24,7 +24,7 @@ app.use(awsServerlessExpressMiddleware.eventContext());
 // Enable CORS for all methods
 //app.use(cors());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Methods", "*");
+  //res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
   next();
