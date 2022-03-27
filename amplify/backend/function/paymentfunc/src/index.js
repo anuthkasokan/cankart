e4d45f6f3bd9a50924b670c0298a97643ea29620
Anuth Asokan <anuthkasokan@gmail.com>
2022-03-27 16:32:53 -0400

    Update index.js

--- amplify/backend/function/paymentfunc/src/index.js
+++ amplify/backend/function/paymentfunc/src/index.js
@@ -1,42 +1,15 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
-const stripe = require("stripe")(
-  "sk_test_51KYPrEE5iK0SDtJgLUzXCziS0LE0dJF3vCoPXmVZGiu6VKU79NijcH4Twf9M7GatyjsnD03V91VOt7Fv0pbTEhTT00FfU4Brjt"
-);
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const awsServerlessExpress = require("aws-serverless-express");
+const app = require("./app");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+/**
+ * @type {import('http').Server}
+ */
+const server = awsServerlessExpress.createServer(app);
 
-app.use(cors());
-
-app.post("/payment", cors(), async (req, res) => {
-  let { amount, id } = req.body;
-
-  try {
-    const payment = await stripe.paymentIntents.create({
-      amount,
-      currency: "cad",
-      description: "New game",
-      payment_method: id,
-      confirm: true,
-    });
-    console.log("payment", payment);
-    res.json({
-      message: "Payment successful",
-      success: true,
-    });
-  } catch (error) {
-    console.log("error", error);
-    res.json({
-      message: "Payment failed",
-      success: false,
-    });
-  }
-});
-
-app.listen(4000, () => {
-  console.log("Server is listening");
-});
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = (event, context) => {
+  console.log(`EVENT: ${JSON.stringify(event)}`);
+  return awsServerlessExpress.proxy(server, event, context, "PROMISE").promise;
+};
